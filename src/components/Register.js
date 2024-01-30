2d2ea0d866f7def435ffc979527c985d36830455
Melike ISIK <melike_gs_8-@hotmail.com>
2024-01-30 16:24:57 +0300

    Hesap oluşturma formu eklendi

--- src/components/Register.js
+++ src/components/Register.js
@@ -7,18 +7,20 @@ import SignUp from './SignUp'
 library.add(faXTwitter, faApple)
 
 function Register() {
-    const [openSignup, setOpenSignup] = useState(false) 
-  
+    const [openForm, setOpenForm] = useState(false) 
   return (
-    <div style={{backgroundColor:openSignup?"rgba(91, 112, 131, 0.4)" : ""}} className={style.registerPage}>
-      <div style={{display:openSignup ? "block" : "none" , opacity:""}} className={style.form}>
+    <div style={{backgroundColor:openForm?"rgba(91, 112, 131, 0.4)" : ""}} className={style.registerPage}>
+      <div style={{display:openForm ? "block" : "none" , opacity:""}} className={style.form}>
+        <div onClick={() =>setOpenForm(false)} className={style.closeForm}>
+          <FontAwesomeIcon icon="fa-solid fa-x" />
+        </div>
         <SignUp/>
       </div>
-      <div style={{opacity:openSignup? "60%" : ""}} className={style.logo}>
+      <div style={{opacity:openForm? "60%" : ""}} className={style.logo}>
         <FontAwesomeIcon icon="fa-brands fa-x-twitter" />
       </div>
       <div className={style.register}>
-        <div style={{opacity:openSignup? "60%" : ""}} className={style.title}>
+        <div style={{opacity:openForm? "60%" : ""}} className={style.title}>
             <span >Şu anda olup</span>
             <span>bitenler</span>
         </div>
@@ -29,19 +31,19 @@ function Register() {
             <img style={{height:"22px", objectFit:"cover"}} src='assets/images/google.png' />
             <span>Google ile kaydolun</span>
         </button>
-        <button style={{opacity:openSignup? "60%" : ""}} className={style.registerBtn}>
+        <button style={{opacity:openForm? "60%" : ""}} className={style.registerBtn}>
             <FontAwesomeIcon style={{fontSize:"22px"}} icon="fa-brands fa-apple" />
             <span style={{fontWeight:700}}>Apple ile kaydolun</span>
         </button>
-        <div style={{opacity:openSignup? "60%" : ""}} className={style.registerSeperate}>
+        <div style={{opacity:openForm? "60%" : ""}} className={style.registerSeperate}>
             <div className={style.seperateLine}></div>
             <div className={style.seperateText}>veya</div>
             <div className={style.seperateLine}></div>
         </div>
-        <button style={{opacity:openSignup? "60%" : ""}} onClick={() => {setOpenSignup(true); console.log("hesap")}} className={style.createAccountBtn}>
+        <button style={{opacity:openForm? "60%" : ""}} onClick={() => {setOpenForm(true); console.log("hesap")}} className={style.createAccountBtn}>
             <span>Hesap oluştur</span>
         </button>
-        <div style={{opacity:openSignup? "60%" : ""}}>
+        <div style={{opacity:openForm? "60%" : ""}}>
             <span >Zaten bir hesabın var mı?</span>
             <button className={style.loginBtn}>
                 <span>Giriş yap</span>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,6 +3,11 @@ import style from "../style.module.css"
 import { useFormik } from 'formik'
 import {db} from "../firebase";
 import {addDoc, collection,getDocs } from "@firebase/firestore"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+library.add(faX)
+
 function SignUp() {
   const users = collection(db, "users")
   const {handleChange, handleSubmit, values} = useFormik({
@@ -30,10 +35,22 @@ function SignUp() {
     <div className={style.signupForm}>
       <h1>Hesabını oluştur</h1>
       <form onSubmit={handleSubmit}>
-        <input type='text' name="userName" value={values.userName}  onChange={handleChange} />
-        <input type='text' name="userSurname" value={values.userSurname}  onChange={handleChange}/>
-        <input type='text' name="userNick" value={values.userNick}  onChange={handleChange}/>
-        <input type='password' name="userPassword" value={values.userPassword}  onChange={handleChange}/>
+        <div className={style.inputBox}>
+          <input  type='text' name="userName" value={values.userName}  onChange={handleChange} />
+          <span>İsim</span>
+        </div>
+        <div className={style.inputBox}>
+          <input  type='text' name="userSurname" value={values.userSurname}  onChange={handleChange}/>       
+          <span>Soyisim</span>
+        </div>
+        <div className={style.inputBox}>
+          <input  type='text' name="userNick" value={values.userNick}  onChange={handleChange}/>
+          <span>Kullanıcı Adı</span>
+        </div>
+        <div className={style.inputBox}>
+          <input type='password' name="userPassword" value={values.userPassword}  onChange={handleChange}/>
+          <span>Parola</span>
+        </div>
         <button>Kaydol</button>
       </form>
     </div>
