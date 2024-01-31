9b747432a4dff5c65ca4452202202b165798b3d2
Melike ISIK <melike_gs_8-@hotmail.com>
2024-01-31 17:44:34 +0300

    Posts compoenentinin gönderi bölümü oluşturuldu.

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import style from  "../style.module.css"
 function Agenda() {
   return (
-    <div>
+    <div className={style.agendaPage}>
       Agenda
     </div>
   )
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,9 +2,12 @@ import React from 'react'
 import style from "../style.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faGear } from '@fortawesome/free-solid-svg-icons'
-library.add(faGear)
+import { faGear, faPhotoFilm, faListUl, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faFaceSmile, faCalendarDays } from '@fortawesome/free-regular-svg-icons'
+import { useUserInfo } from '../context/UserInfoContext'
+library.add(faGear,faPhotoFilm,faListUl,faFaceSmile,faCalendarDays,faLocationDot)
 function Posts() {
+  const {userInfo} = useUserInfo()
   return (
     <div className={style.posts}>
         <div className={style.postsHeader}>
@@ -12,11 +15,26 @@ function Posts() {
           <span>Takip edilenler</span>
           <span><FontAwesomeIcon icon="fa-solid fa-gear" /></span>
         </div>
-        <div className={style.createPost}>
+        <div className={style.postPart}>
+          <div className={style.createPost}>
+            <div className={style.profileImgContainer}>
+                <img  src={`https://api.multiavatar.com/${userInfo.userNick}.png`}/>
+            </div>
+            <textarea placeholder='Neler oluyor?'></textarea>
+            <div className={style.categories}>
+              <ul>
+                <li><FontAwesomeIcon icon="fa-solid fa-photo-film" /></li>
+                <li><FontAwesomeIcon icon="fa-solid fa-list-ul" /></li>
+                <li><FontAwesomeIcon icon="fa-regular fa-face-smile" /></li>
+                <li><FontAwesomeIcon icon="fa-regular fa-calendar-days" /></li>
+                <li><FontAwesomeIcon icon="fa-solid fa-location-dot" /></li>
+              </ul>
+            </div>
+            <button className={style.send}>Gönder</button>
+          </div>
+          <div className={style.postsList}>
 
-        </div>
-        <div className={style.postsList}>
-
+          </div>
         </div>
     </div>
 
