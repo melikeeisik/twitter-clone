820edba4d6945d898c2b0479c37ddb9acf189233
Melike ISIK <melike_gs_8-@hotmail.com>
2024-01-31 15:34:32 +0300

    Posts componentinin headerı hazırlandı.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import style from "../style.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
+library.add(faGear)
 function Posts() {
   return (
     <div className={style.posts}>
-        Posts
+        <div className={style.postsHeader}>
+          <span>Sana özel</span>
+          <span>Takip edilenler</span>
+          <span><FontAwesomeIcon icon="fa-solid fa-gear" /></span>
+        </div>
+        <div className={style.createPost}>
+
+        </div>
+        <div className={style.postsList}>
+
+        </div>
     </div>
 
   )
