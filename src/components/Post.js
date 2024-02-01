f8ece914b2718de423b42b296e9bbd8804df194d
Melike ISIK <melike_gs_8-@hotmail.com>
2024-02-01 10:02:35 +0300

    Post kutucukları güncellenmiştir

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import style from "../style.module.css"
-import { useUserInfo } from '../context/UserInfoContext'
 function Post({ post}) {
   return (
     <div className={style.postBox}>
-        <div className={style.userProfile}>
-            <img  src={`https://api.multiavatar.com/${post.userNick}.png`}/>
-        </div>
-        <div className={style.postContainer}>
+        <div className={style.userInfoBox}>
+            <div className={style.userProfile}>
+                <img  src={`https://api.multiavatar.com/${post.userNick}.png`}/>
+            </div>
             <div className={style.userNameBox}>
                 <span style={{fontWeight:700}}>{post.userName}</span>
                 <span style={{fontWeight:700}}>{post.userSurname}</span>
                 <span style={{color:"#5c5b5b"}}>@{post.userNick}</span>
             </div>
-            <div className={style.userPostBox}>
-                <span>{post.userPost}</span>
-            </div>
+        </div>
+        <div className={style.userPostBox}>
+            <p>{post.userPost}</p>
         </div>
     </div>
   )
