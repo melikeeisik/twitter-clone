07680bfbfc43d336cd7b82567f517c54174939a4
Melike ISIK <melike_gs_8-@hotmail.com>
2024-02-01 12:11:35 +0300

    Arama inputu eklendi

--- src/components/Home/Agenda/Agenda.js
+++ src/components/Home/Agenda/Agenda.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import style from  "../../../style.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+library.add(faMagnifyingGlass)
+
 function Agenda() {
   return (
     <div className={style.agendaPage}>
-      Agenda
+      <div className={style.searchBox}>
+        <input type='text' placeholder='Ara' /> 
+        <div>
+          <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+        </div>
+      </div>
     </div>
   )
 }
