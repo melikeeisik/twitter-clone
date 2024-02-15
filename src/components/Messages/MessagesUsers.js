4bd17fcbda7e070f9ce526e774a0af8940110084
Melike ISIK <melike_gs_8-@hotmail.com>
2024-02-15 14:23:14 +0300

    Kullanıcı arama özelliği eklendi

--- src/components/Messages/MessagesUsers.js
+++ src/components/Messages/MessagesUsers.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoCloseOutline } from "react-icons/io5";
 import { IoIosSearch } from "react-icons/io";
 import style from "../../style.module.css"
 function MessagesUsers({userList,setPageVisible}) {
+    const [users, setUsers] = useState([])
+    const [searchName, setSearchName] = useState("")
+
+    useEffect(() =>{
+        const searchNameLower = searchName.toLocaleLowerCase()
+        const newUsers = userList.filter(user => user.userName.toLocaleLowerCase().includes(searchNameLower));
+        if(newUsers.length > 0){
+            setUsers(newUsers)
+        }
+    }, [searchName])
+
   return (
     <div style={{padding:"10px 15px"}}>
       <div >
         <div>
             <div style={{display:"flex", justifyContent:"space-between", marginBottom:5}}>
                 <div style={{display:"flex", gap:"10px", alignItems:"center"}}>
-                    <div onClick={() => setPageVisible(false)} >
+                    <div onClick={() => {setPageVisible(false); setSearchName("");setUsers(userList)}} >
                         <IoCloseOutline style={{fontSize:25}} />
                     </div>
                     <span style={{fontWeight:700, fontSize:20}}>Yeni mesaj</span>
@@ -17,7 +28,7 @@ function MessagesUsers({userList,setPageVisible}) {
                 <button style={{padding:"7px 20px", borderRadius:20, backgroundColor:"#fff", border:"1px solid #fff", fontWeight:700, fontSize:14}}>Sonraki</button>
             </div>
             <div className={style.searchMessageUser}>
-                <input placeholder='Kişi ara' />
+                <input value={searchName} placeholder='Kişi ara' onChange={(e) => setSearchName(e.target.value)} />
                 <IoIosSearch />
             </div>
         </div>
@@ -25,7 +36,7 @@ function MessagesUsers({userList,setPageVisible}) {
       <div>
         <ul style={{margin:0,padding:0,listStyle:"none"}}>
             {
-                userList.map((user,index) =>{
+                users.map((user,index) =>{
                     return(
                         <li style={{display:"flex", gap:"10px", padding:"10px 0px"}} key={index}>
                             <img style={{width:"40px", objectFit:"contain"}} src={`https://api.multiavatar.com/${user.userNick}.png`} alt='Profile Picture' />
